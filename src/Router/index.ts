872bcb3b0dcb922b155a915cb638ea26eabeeb6a
Erik Lopes <erik.lopesoliveira2019@gmail.com>
2022-08-18 22:02:13 -0300

    chore(Router): finalizando função addRoutes, para criar todas as rotas de um crud completo

--- src/Router/index.ts
+++ src/Router/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import Controller from '../Controllers/Controller';
 
 class CustomRouter<T> {
   public router: Router;
@@ -7,7 +8,15 @@ class CustomRouter<T> {
     this.router = Router();
   }
 
-  public addRoutes(): void {
+  public addRoutes(
+    controller: Controller<T>,
+    route: string = controller.route
+  ): void {
+    this.router.post(`${route}`, controller.create);
+    this.router.get(`${route}`, controller.read);
+    this.router.get(`${route}/:id`, controller.readOne);
+    this.router.put(`${route}/:id`, controller.update);
+    this.router.delete(`${route}/:id`, controller.delete);
   }
 }
 
