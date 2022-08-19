aa5154d580b3027b06b491899228a864a927b004
Erik Lopes <erik.lopesoliveira2019@gmail.com>
2022-08-18 22:14:11 -0300

    chore(CustomeInterface): criando CustomerInterface

---
+++ src/Interfaces/CustomerInterface.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+// eslint-disable-next-line no-useless-escape
+const PHONE_REGEX = /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/;
+
+export const customerSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  phone: z.string().regex(PHONE_REGEX),
+  password: z.string().min(6)
+});
+
+export type Customer = z.infer<typeof customerSchema>;
