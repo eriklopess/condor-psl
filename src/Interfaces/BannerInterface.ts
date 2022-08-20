6a4837f2e3142b932c76f97a0cc5b672fc419458
Erik Lopes <erik.lopesoliveira2019@gmail.com>
2022-08-20 17:19:21 -0300

    refactor(BannerInterface, CustomerInterface): Adicionando mensagens de erro personalizadas

--- src/Interfaces/BannerInterface.ts
+++ src/Interfaces/BannerInterface.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 
 export const bannerSchema = z.object({
   name: z.string({
-    required_error: 'Name is required.',
-    invalid_type_error: 'Name must be a string.'
-  }).min(3, { message: 'Name must be at least 3 characters long.' }),
+    required_error: 'name is required.',
+    invalid_type_error: 'name must be a string.'
+  }).min(3, { message: 'name must be at least 3 characters long.' }),
   image: z.string().optional(),
   customerID: z.any(),
   endAt: z.string({
@@ -19,23 +19,4 @@ export const bannerSchema = z.object({
   _id: z.any().optional()
 });
 
-export const bannerSchemaUpdate = z.object({
-  name: z.string({
-    required_error: 'Name is required.',
-    invalid_type_error: 'Name must be a string.'
-  }).min(3, { message: 'Name must be at least 3 characters long.' }),
-  image: z.string().optional(),
-  customerID: z.any(),
-  endAt: z.string({
-    invalid_type_error: 'endAt must be a string.',
-    required_error: 'endAt is required.'
-  }),
-  startAt: z.string({
-    invalid_type_error: 'startAt must be a string.',
-    required_error: 'startAt is required.'
-  }),
-  status: z.boolean().optional()
-});
-
 export type Banner = z.infer<typeof bannerSchema>;
-export type BannerUpdate = z.infer<typeof bannerSchemaUpdate>;
--- src/Interfaces/CustomerInterface.ts
+++ src/Interfaces/CustomerInterface.ts
@@ -4,10 +4,22 @@ import { z } from 'zod';
 const PHONE_REGEX = /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/;
 
 export const customerSchema = z.object({
-  name: z.string().min(3, { message: 'Name must be at least 3 characters long.' }),
-  email: z.string().email({ message: 'Please enter a valid email.' }),
-  phone: z.string().regex(PHONE_REGEX, { message: 'Please enter a valid phone number.' }),
-  password: z.string().min(6, { message: 'Password must be at least 6 characters long.' }),
+  name: z.string({
+    required_error: 'name is required.',
+    invalid_type_error: 'name must be a string.'
+  }).min(3, { message: 'name must be at least 3 characters long.' }),
+  email: z.string({
+    required_error: 'email is required.',
+    invalid_type_error: 'email must be a string.'
+  }).email({ message: 'Please enter a valid email.' }),
+  phone: z.string({
+    required_error: 'phone is required.',
+    invalid_type_error: 'phone must be a string.'
+  }).regex(PHONE_REGEX, { message: 'Please enter a valid phone number.' }),
+  password: z.string({
+    required_error: 'password is required.',
+    invalid_type_error: 'password must be a string.'
+  }).min(6, { message: 'Password must be at least 6 characters long.' }),
   banners: z.any().optional()
 });
 
