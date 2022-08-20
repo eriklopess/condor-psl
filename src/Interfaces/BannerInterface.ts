a4d8fb3c5270f97753b96798f85876f5053f37ee
Erik Lopes <erik.lopesoliveira2019@gmail.com>
2022-08-20 17:13:57 -0300

    refactor(BannerInterface, CustomerInterface): Adicionando mensagens de erro personalizadas

--- src/Interfaces/BannerInterface.ts
+++ src/Interfaces/BannerInterface.ts
@@ -1,23 +1,40 @@
 import { z } from 'zod';
 
 export const bannerSchema = z.object({
-  name: z.string(),
+  name: z.string({
+    required_error: 'Name is required.',
+    invalid_type_error: 'Name must be a string.'
+  }).min(3, { message: 'Name must be at least 3 characters long.' }),
   image: z.string().optional(),
   customerID: z.any(),
-  endAt: z.string(),
-  startAt: z.string(),
+  endAt: z.string({
+    invalid_type_error: 'endAt must be a string.',
+    required_error: 'endAt is required.'
+  }),
+  startAt: z.string({
+    invalid_type_error: 'startAt must be a string.',
+    required_error: 'startAt is required.'
+  }),
   status: z.boolean(),
   _id: z.any().optional()
 });
 
 export const bannerSchemaUpdate = z.object({
-  name: z.string().optional(),
+  name: z.string({
+    required_error: 'Name is required.',
+    invalid_type_error: 'Name must be a string.'
+  }).min(3, { message: 'Name must be at least 3 characters long.' }),
   image: z.string().optional(),
-  customerID: z.any().optional(),
-  endAt: z.string().optional(),
-  startAt: z.string().optional(),
-  status: z.boolean().optional(),
-  _id: z.any().optional()
+  customerID: z.any(),
+  endAt: z.string({
+    invalid_type_error: 'endAt must be a string.',
+    required_error: 'endAt is required.'
+  }),
+  startAt: z.string({
+    invalid_type_error: 'startAt must be a string.',
+    required_error: 'startAt is required.'
+  }),
+  status: z.boolean().optional()
 });
 
 export type Banner = z.infer<typeof bannerSchema>;
--- src/Interfaces/CustomerInterface.ts
+++ src/Interfaces/CustomerInterface.ts
@@ -4,10 +4,10 @@ import { z } from 'zod';
 const PHONE_REGEX = /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/;
 
 export const customerSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  phone: z.string().regex(PHONE_REGEX),
-  password: z.string().min(6),
+  name: z.string().min(3, { message: 'Name must be at least 3 characters long.' }),
+  email: z.string().email({ message: 'Please enter a valid email.' }),
+  phone: z.string().regex(PHONE_REGEX, { message: 'Please enter a valid phone number.' }),
+  password: z.string().min(6, { message: 'Password must be at least 6 characters long.' }),
   banners: z.any().optional()
 });
 
